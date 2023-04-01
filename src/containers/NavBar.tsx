0c09be92031b55cf4a4c5f63c8de3734b947f651
tojonir <tojonirinandrianarijaona@gmail.com>
2023-04-01 17:09:49 +0300

    tailwind setup

---
+++ src/containers/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FC } from "react";
+
+const NavBar: FC = () => {
+  return (
+    <nav className="h-[70px]">
+      <div></div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-
+import NavBar from "../containers/NavBar";
 const IndexPage: React.FC<PageProps> = () => {
-  return <main></main>;
+  return (
+    <main>
+      <NavBar />
+      <div className="w-full h-screen flex justify-center items-center">
+        <div>
+          <h1>Front-end kit for React app</h1>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default IndexPage;
