fb42b3592c27be4c046b2b0f7229ac0c43d4f402
tojonir <tojonirinandrianarijaona@gmail.com>
2023-04-18 11:05:27 +0300

    setup doclink constant

--- src/containers/DocLayout.tsx
+++ src/containers/DocLayout.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from "react";
 import React from "react";
 import Layout from "./Layout";
 import { Link } from "gatsby";
+import { doclink } from "../utils/constant";
 
 interface DocLayoutProps {
   children: ReactNode;
@@ -10,14 +11,16 @@ const DocLayout: FC<DocLayoutProps> = ({ children }) => {
   return (
     <Layout>
       <div className="w-full h-full flex mt-5">
-        <div className="w-[200px] p-2">
-          <Link
-            activeClassName="bg-gray-900 text-purple"
-            to={`/doc/`}
-            className="px-3 py-2 block w-full hover:bg-gray-900"
-          >
-            {"link"}
-          </Link>
+        <div className="w-[200px] p-1">
+          {doclink.map((doc) => (
+            <Link
+              activeClassName="bg-gray-900 text-purple"
+              to={`/doc/${doc}`}
+              className="px-3 py-2 block w-full hover:bg-gray-900"
+            >
+              {doc}
+            </Link>
+          ))}
         </div>
         <div className="grow p-5">{children}</div>
       </div>
---
+++ src/utils/constant.ts
@@ -0,0 +1,8 @@
+export const doclink = [
+  "overview",
+  "installation",
+  "Layout",
+  "Row",
+  "Col",
+  "Grid",
+];
