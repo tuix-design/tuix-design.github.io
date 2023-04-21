7069835b92714deb6018ad9543efcd4bfc4bc3c1
tojonir <tojonirinandrianarijaona@gmail.com>
2023-04-21 17:14:57 +0300

    add icon

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 import { Link } from "gatsby";
 import React, { FC, useCallback, useState } from "react";
-import { Modal } from "tuix-design";
+import { Icon, Modal } from "tuix-design";
 import { index, tag, tagSchema } from "../utils/constant";
 import { search } from "../utils/helper";
 
@@ -44,22 +44,36 @@ const Search: FC<SearchProps> = () => {
         search ctrl+k
       </div>
       <div className="w-1/2 bg-dark mt-32 main-radius">
-        <input
-          placeholder="search"
-          className="bg-transparent outline-none w-full border-b p-5 rounded-none"
-          onChange={(e) => onSearch(e.target.value)}
-          autoFocus
-          onFocus={(e) => onSearch(e.target.value)}
-        />
+        <div className="flex items-center border-b p-5">
+          <Icon name="Search" color="#f2f2f2" />
+
+          <input
+            placeholder="search"
+            className="bg-transparent outline-none w-full ml-5 rounded-none"
+            onChange={(e) => onSearch(e.target.value)}
+            autoFocus
+            onFocus={(e) => onSearch(e.target.value)}
+          />
+        </div>
+
         <div className="flex flex-col p-5 max-h-[300px] overflow-y-auto">
           {tags.map((tag, i) => {
+            const id = tag.link.includes("#");
             return (
               <Link
                 key={i}
                 to={`/doc/${tag.link}`}
-                className="search-result-show h-12 flex items-center  px-2 my-2 main-radius bg-gray-900 hover:bg-purple"
+                className={`search-result-show h-12 flex items-center  px-2 my-2 ${
+                  id && "pl-4"
+                } main-radius bg-gray-900 hover:bg-purple`}
               >
-                <span>{tag.label}</span>
+                <span>
+                  {<Icon name={id ? "Down" : "File"} size={32} color="#fff" />}
+                </span>
+                <span>
+                  {id && "# "}
+                  {tag.label}
+                </span>
               </Link>
             );
           })}
