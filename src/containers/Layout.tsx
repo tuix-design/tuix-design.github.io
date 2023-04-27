d4f04252f414e6e5140775b7924e6cbaf4ba1617
tojonir <tojonirinandrianarijaona@gmail.com>
2023-04-27 23:22:07 +0300

    home responsive

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -9,9 +9,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <main className="">
       <NavBar />
-      {/* <div className="max-w-[1200px] m-auto h-[calc(100vh-50px)] px-5">
+      <div className="max-w-[1200px] m-auto h-[calc(100vh-50px)] px-5">
         {children}
-      </div> */}
+      </div>
     </main>
   );
 };
