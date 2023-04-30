29886f114e9b5a5164e7eeefe5909d734c5e8473
tojonir <tojonirinandrianarijaona@gmail.com>
2023-04-30 22:33:05 +0300

    done

--- src/pages/doc/button.tsx
+++ src/pages/doc/button.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import DocLayout from "../../containers/DocLayout";
 import React from "react";
-import { Code } from "tuix-design";
+import { Button, Code, Icon } from "tuix-design";
 import ApiTable from "../../containers/ApiTable";
 import { apiButton } from "../../utils/constant";
 
@@ -11,7 +11,15 @@ const button: FC = (props) => {
   return (
     <DocLayout
       title="button"
-      tag={["basic", "icon button", "size", "api"]}
+      tag={[
+        "basic",
+        "icon button",
+        "size",
+        "disabled",
+        "bordered",
+        "rounded",
+        "api",
+      ]}
       next="icon"
       prev="installation"
     >
@@ -22,8 +30,88 @@ const button: FC = (props) => {
         mode="dark"
         language="javascript"
       >{`import {Button} from "tuix-design"`}</Code>
+      <div className="my-3 flex gap-4">
+        <Button>primary</Button>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="success">success</Button>
+        <Button variant="warning">warning</Button>
+        <Button variant="danger">danger</Button>
+      </div>
 
-      <ApiTable data={apiButton} />
+      <Code mode="dark" language="javascript">
+        {`<Button>primary</Button>
+<Button variant="secondary">secondary</Button>
+<Button variant="success">success</Button>
+<Button variant="warning">warning</Button>
+<Button variant="danger">danger</Button>`}
+      </Code>
+
+      <p id="icon-button" className="py-5">
+        Icon Button
+      </p>
+      <div className="my-3 flex gap-4">
+        <Button icon={<Icon name="Home" color="#fff" />}>primary</Button>
+      </div>
+
+      <Code mode="dark" language="javascript">
+        {`<Button icon={<Icon name="Home" color="#fff" />}>primary</Button>`}
+      </Code>
+      <p id="size" className="py-5">
+        Size
+      </p>
+      <div className="my-3 flex gap-4">
+        <Button size="sm" icon={<Icon size={16} name="Home" color="#fff" />}>
+          small
+        </Button>
+        <Button icon={<Icon name="Home" color="#fff" />}>medium</Button>
+        <Button size="lg" icon={<Icon name="Home" color="#fff" />}>
+          large
+        </Button>
+      </div>
+
+      <Code mode="dark" language="javascript">
+        {`<Button size="sm" icon={<Icon size={16} name="Home" color="#fff" />}>
+  small
+</Button>
+<Button icon={<Icon name="Home" color="#fff" />}>medium</Button>
+<Button size="lg" icon={<Icon name="Home" color="#fff" />}>
+  large
+</Button>`}
+      </Code>
+
+      <p id="disabled" className="py-5">
+        Disabled
+      </p>
+      <div className="my-3 flex gap-4">
+        <Button disabled>disable</Button>
+      </div>
+
+      <Code mode="dark" language="javascript">
+        {`<Button disabled>disable</Button>`}
+      </Code>
+
+      <p id="bordered" className="py-5">
+        Bordered
+      </p>
+      <div className="my-3 flex gap-4">
+        <Button bordered>bordered</Button>
+      </div>
+
+      <Code mode="dark" language="javascript">
+        {`<Button bordered>bordered</Button>`}
+      </Code>
+      <p id="rounded" className="py-5">
+        Rounded
+      </p>
+      <div className="my-3 flex gap-4">
+        <Button rounded icon={<Icon name="Home" color="#fff" />}></Button>
+      </div>
+
+      <Code mode="dark" language="javascript">
+        {`<Button rounded icon={<Icon name="Home" color="#fff" />}></Button>`}
+      </Code>
+
+      <ApiTable id="api" data={apiButton} />
     </DocLayout>
   );
 };
